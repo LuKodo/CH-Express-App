64db1beba942f85702132157a215fc64ad6b029b
Luis Caraballo <lcm7039@gmail.com>
2024-08-13 15:03:42 -0500

    login task

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -33,14 +33,12 @@ export const Login = () => {
 
     const handleLogin = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
-        console.log(username, password);
-
         if (username !== "" && password !== "") {
             const user = users.find((user) => user.usuario === username && user.contrasena === password);
 
             if (user) {
                 localStorage.setItem("user", JSON.stringify(user));
-                navigate('/');
+                navigate('/destination');
             } else {
                 setAlertMessage({
                     header: "Login Failed",
@@ -61,49 +59,47 @@ export const Login = () => {
     }
 
     return (
-        <IonContent className="ion-padding">
-            <IonImg
-                src="logo.jpg"
-                alt="logo"
-                className="mt-5 img-fluid"
-            />
+        <div className="page-content header-clear-medium">
+            <div className="card card-style">
+                <div className="content">
+                    <IonImg
+                        src="logo.jpg"
+                        alt="logo"
+                        className="mt-5 img-fluid"
+                    />
 
-            <IonItem className="mt-5">
-                <IonInput
-                    label="Usuario"
-                    labelPlacement="floating"
-                    onIonInput={(e) => setUsername(e.detail.value!)}
-                    value={username}
-
-                />
-            </IonItem>
-
-            <IonItem className="mt-4">
-                <IonInput
-                    label="Contraseña"
-                    labelPlacement="floating"
-                    type="password"
-                    onIonInput={(e) => setPassword(e.detail.value!)}
-                    value={password}
-                />
-            </IonItem>
-            <IonButton
-                id="present-alert"
-                color="warning"
-                expand="block"
-                className="fw-bold mt-5"
-                onClick={handleLogin}
-            >
-                Ingresar
-            </IonButton>
-
-            <IonAlert
-                isOpen={showAlert}
-                header={alertMessage.header}
-                message={alertMessage.message}
-                buttons={alertMessage.buttons}
-                onDidDismiss={() => setShowAlert(false)}
-            />
-        </IonContent>
+                    <div className="form-custom form-label form-icon mb-3">
+                        <i className="bi bi-person-circle font-14"></i>
+                        <input
+                            type="text"
+                            className="form-control rounded-xs"
+                            id="c1"
+                            placeholder="JohnDoe"
+                            onChange={(e) => setUsername(e.target.value!)}
+                            onKeyDown={(e) => e.key === "Enter" && handleLogin(e)}
+                            value={username}
+                        />
+                        <label htmlFor="c1" className="color-theme">Nombre de usuario</label>
+                        <span>(requerido)</span>
+                    </div>
+                    <div className="form-custom form-label form-icon mb-3">
+                        <i className="bi bi-asterisk font-12"></i>
+                        <input
+                            type="password"
+                            className="form-control rounded-xs"
+                            id="c2"
+                            placeholder="Password"
+                            onKeyDown={(e) => e.key === "Enter" && handleLogin(e)}
+                            onChange={(e) => setPassword(e.target.value!)}
+                            value={password}
+                        />
+                        <label htmlFor="c2" className="color-theme">Contraseña</label>
+                        <span>(requerido)</span>
+                    </div>
+                    <button
+                        onClick={handleLogin} className="btn w-100 rounded-sm btn-m gradient-yellow text-uppercase font-700 mt-4 mb-3 btn-full shadow-bg shadow-bg-s">Ingresar</button>
+                </div>
+            </div>
+        </div>
     )
 }
